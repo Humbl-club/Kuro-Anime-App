3e4cb9669f1ef30ab026701a6da08b2291da3e03
Max Hufschlag <max.hufschlag@googlemail.com>
2025-09-29 13:36:55 +0200

    Initial Commit

---
+++ Kuro/KuroApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuroApp.swift
+//  Kuro
+//
+//  Created by Max Dev on 29.09.25.
+//
+
+import SwiftUI
+
+@main
+struct KuroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KuroTests/KuroTests.swift
@@ -0,0 +1,16 @@
+//
+//  KuroTests.swift
+//  KuroTests
+//
+//  Created by Max Dev on 29.09.25.
+//
+
+import Testing
+
+struct KuroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
